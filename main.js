caa75a93d8fa94e675ea8c79bdc68723599406d2
Kevin Ruiz Molina <kevinrumolina26@gmail.com>
2021-05-16 11:37:16 -0500

    set slider via keyframes

--- main.js
+++ main.js
@@ -6,13 +6,5 @@ const closeForm = () => formInput.checked = false;
 
 closeButton.addEventListener('click', closeForm);
 
-/*image slider*/
-const imagesBaseSrc = './assets/'
-const sliderContainer = document.querySelector('.header');
-let intervalCounter = 1;
 
-setInterval(function() {
-    sliderContainer.style.backgroundImage = `url("../assets/images/slide${intervalCounter}.jpg")`
-    intervalCounter === 4 ? intervalCounter = 1 : intervalCounter += 1;
-},8000);
 
