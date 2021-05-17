214d9701e1b71004a80e7521111f00840a9cbd3f
Kevin Ruiz Molina <kevinrumolina26@gmail.com>
2021-05-17 17:08:16 -0500

    set dark background in page when form is open

--- sources/closeButton.js
+++ sources/closeButton.js
@@ -1,8 +1,13 @@
 /*Boton de Cerrar formulario*/
 const closeButton = document.querySelector('.close-button')
+const bookNowButton = document.querySelector('.form-container__title')
 const formInput = document.querySelector('#form');
+const darkBackground = document.querySelector('.dark-background');
 
-const closeForm = () => formInput.checked = false;
+const closeForm = () => {
+    darkBackground.style.visibility = "hidden"
+    formInput.checked = false;
+}
 
 closeButton.addEventListener('click', closeForm);
 
